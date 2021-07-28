4c21a0d1c5480aaf97a9b029a41bd393ef1a7ab3
Godgiver <otanriverdi23@my.sevkoleji.k12.tr>
2021-07-28 23:15:48 +0300

    Feeder kodu yazıldı
    constant'a 2 değer atadım

--- src/main/java/com/team6838/subsystems/feeder.java
+++ src/main/java/com/team6838/subsystems/feeder.java
@@ -8,11 +8,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class feeder extends SubsystemBase {
   /** Creates a new feeder. */
-  public feeder() {}
+// This method will be called once per scheduler run
+  private WPI_TalonSRX feeder = new WPI_TalonSRX(Constants.k_feederPort);
+  
+  public void feederIn() {feederIn(1);};
+  public void feederIn(double scale) {
+    feeder.set(Constants.feederSpeed * scale);
+  }
+
+  public void feederOut() {feederOut(1);};
+  public void feederOut(double scale) {
+    feeder.set(Constants.feederSpeed * scale);
+  }
+  public void stopEverything() {
+    feeder.set(0);
+  }
+
+
+
+  /*
+  void runFeeder(double güç[-1,1])
+
+  void runFeederForward()
+  void runFeederBackwards()
+
+  boolean isJammed()
+  check if the current is above a number
+  return true/false
+  */
+  public feeder() { }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
     
   }
 }
